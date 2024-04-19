b082ce40e92bd41fe190dd17438e5bd0f2ab27c7
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-04-18 22:28:39 -0300

    producto de matrices en paralelo

--- Unidad 3/Parte 3/producto_matrices.c
+++ Unidad 3/Parte 3/producto_matrices.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 10
+#include <omp.h>
+#define N 1000
+
+int matriz1[N][N];
+int matriz2[N][N];
+int resultante[N][N];
 
 void cargar_arreglo(int arr[N][N])
 {
@@ -46,26 +51,50 @@ void producto_matrices(int m1[N][N], int m2[N][N], int resultante[N][N])
 			for (int k = 0; k < N; k++)
 			{
 				resultante[i][j] = resultante[i][j] + (m1[i][k] * m2[k][j]);
+				// resultante[i][j] += (m1[i][k] * m2[k][j]);
 			}
 		}
 	}
 }
 
-int main()
+double multiplicacion_matrices(int matriz1[N][N], int matriz2[N][N], int resultante[N][N])
 {
-	int matriz1[N][N];
-	int matriz2[N][N];
-	int resultante[N][N];
+	double inicio, fin;
+	inicio = omp_get_wtime();
+
 	cargar_arreglo(matriz1);
 	cargar_arreglo(matriz2);
-	printf("Mostramos la matriz 1:\n");
-	mostrar_arreglo(matriz1);
-	printf("\nMostramos la matriz 2:\n");
-	mostrar_arreglo(matriz2);
+	// printf("Mostramos la matriz 1:\n");
+	// mostrar_arreglo(matriz1);
+	// printf("\nMostramos la matriz 2:\n");
+	// mostrar_arreglo(matriz2);
 
 	cerear_matriz(resultante);
 	producto_matrices(matriz1, matriz2, resultante);
-	printf("\nMostramos la matriz resultante:\n");
-	mostrar_arreglo(resultante);
+	// printf("\nMostramos la matriz resultante:\n");
+	// mostrar_arreglo(resultante);
+
+	fin = omp_get_wtime();
+	return fin - inicio;
+}
+
+int main()
+{
+	double tiempos[10], tpromedio;
+
+	for (int i = 0; i < 10; i++)
+	{ // realizo diez pasadas
+		tiempos[i] = multiplicacion_matrices(matriz1, matriz2, resultante);
+		// printf("\nTiempo de la pasada %d es: %.6f seg\n", i+1, tiempos[i]);
+	}
+
+	// Para sacar tiempo ponderado (entre 2da y 9na pasada)
+	for (int i = 1; i < 9; i++)
+	{
+		tpromedio += tiempos[i];
+	}
+	tpromedio /= 8;
+	printf("\nTiempo promedio de ejecucion secuencial: %.8f seg", tpromedio);
+
 	return 0;
 }
--- Unidad 3/Parte 3/producto_matrices_paralelo.c
+++ Unidad 3/Parte 3/producto_matrices_paralelo.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define N 10
+#define N 1000
+
+int matriz1[N][N];
+int matriz2[N][N];
+int resultante[N][N];
+
+// Vario el numero de cores: 1,2,3 y 4 para esta compu gamer (esto para el miercoles xd)
 
 void cargar_arreglo(int arr[N][N])
 {
 	int i, j;
-// double inicio, fin;
-// inicio = omp_get_wtime();
-#pragma omp parallel for private(i, j) shared(arr)
+	// double inicio, fin;
+	// inicio = omp_get_wtime();
+	omp_set_num_threads(8);						   // para que ejecute con 8 cores
+#pragma omp parallel for private(i, j) shared(arr) // Carga paralelizable pues cada hilo genera numeros aleatorios para la matriz
 	for (i = 0; i < N; i++)
 	{
 		for (j = 0; j < N; j++)
@@ -39,6 +46,7 @@ void mostrar_arreglo(int arr[N][N])
 void cerear_matriz(int m[N][N])
 {
 	int i, j;
+	omp_set_num_threads(8); // para que ejecute con 8 cores
 #pragma omp parallel for private(i, j) shared(m)
 	for (i = 0; i < N; i++)
 	{
@@ -52,6 +60,7 @@ void cerear_matriz(int m[N][N])
 void producto_matrices(int m1[N][N], int m2[N][N], int resultante[N][N])
 {
 	int i, j, k;
+	omp_set_num_threads(8); // para que ejecute con 8 cores
 #pragma omp parallel for collapse(2) private(k)
 	for (i = 0; i < N; i++)
 	{
@@ -60,27 +69,52 @@ void producto_matrices(int m1[N][N], int m2[N][N], int resultante[N][N])
 			for (k = 0; k < N; k++)
 			{
 				resultante[i][j] += m1[i][k] * m2[k][j];
+				// resultante[i][j] += (m1[i][k] * m2[k][j]);
 			}
 		}
 	}
 }
 
-int main()
+double multiplicacion_matrices(int matriz1[N][N], int matriz2[N][N], int resultante[N][N])
 {
-	int matriz1[N][N];
-	int matriz2[N][N];
-	int resultante[N][N];
+	double inicio, fin;
+	inicio = omp_get_wtime();
+
 	cargar_arreglo(matriz1);
 	cargar_arreglo(matriz2);
-	printf("Mostramos la matriz 1:\n");
-	mostrar_arreglo(matriz1);
-	printf("\nMostramos la matriz 2:\n");
-	mostrar_arreglo(matriz2);
+	// printf("Mostramos la matriz 1:\n");
+	// mostrar_arreglo(matriz1);
+	// printf("\nMostramos la matriz 2:\n");
+	// mostrar_arreglo(matriz2);
 
 	cerear_matriz(resultante);
 	producto_matrices(matriz1, matriz2, resultante);
-	printf("\nMostramos la matriz resultante:\n");
-	mostrar_arreglo(resultante);
+	// printf("\nMostramos la matriz resultante:\n");
+	// mostrar_arreglo(resultante);
+
+	fin = omp_get_wtime();
+	return fin - inicio;
+}
+
+int main()
+{
+
+	double tiempos[10], tpromedio;
+
+	for (int i = 0; i < 10; i++)
+	{ // realizo diez pasadas
+		tiempos[i] = multiplicacion_matrices(matriz1, matriz2, resultante);
+		// printf("\nTiempo de la pasada %d es: %.6f seg\n", i+1, tiempos[i]);
+	}
+
+	// Para sacar tiempo ponderado (entre 2da y 9na pasada)
+	for (int i = 1; i < 9; i++)
+	{
+		tpromedio += tiempos[i];
+	}
+	tpromedio /= 8;
+	printf("\nTiempo promedio de ejecucion paralelo: %.8f seg", tpromedio);
+
 	return 0;
 }
 
