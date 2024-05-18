acc7cc84d35c4313360c32776d7e103cacf52e69
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-05-18 15:38:57 -0300

    Unidad 7

---
+++ Unidad 7/ejercicio4_comm_sincrona.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+	int rank, size;
+	clock_t start, end;
+	double cpu_time_used;
+	
+	MPI_Init(&argc, &argv);
+	start = clock();
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	/*if (size < 2) {
+		printf("Se necesitan al menos 2 procesos para la comunicacion\n");
+		MPI_Finalize();
+		return 0;
+	} */
+	int dato = 777;
+	if (rank == 0) {
+		MPI_Send(&dato, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		printf("Proceso %d envia el dato %d\n", rank, dato);
+	} else if (rank == 1) {
+		MPI_Recv(&dato, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Proceso %d recibe el dato %d\n", rank, dato);
+	}
+	MPI_Finalize();
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;		
+	//CLOCKS_PER_SEC constante que indica numero de ciclos por seg de la CPU
+	
+	printf("Tiempo de ejecucion: %.6f segundos\n", cpu_time_used);
+	
+	return 0;
+}
---
+++ Unidad 7/ejercicio6.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define ROOT 0
+#define ARRAY_SIZE 8
+#define PORTION_SIZE 2
+
+int main(int argc, char **argv) {
+	int rank, size;
+	int value;
+	int array[ARRAY_SIZE];
+	int recv_portion[PORTION_SIZE];
+	int gathered_array[ARRAY_SIZE];
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	
+	//1: El proceso raiz envi­a un valor a todos los procesos
+	if (rank == ROOT) {
+		value = 777;  // Valor a enviar
+	}
+	MPI_Bcast(&value, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+	printf("\nPaso 1: Id Proceso %d recibe el valor %d\n", rank, value);
+	
+	//Para Paso 2 y 3: Inicializar el arreglo en el proceso raiz
+	if (rank == ROOT) {
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			array[i] = rand() % 9;		//llenar el arreglo con numeros aleatorios entre 0 y 9
+		}
+		printf("\nSe muestra el arreglo generado por Raiz: ");
+		printf("[");
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			printf(" %d", array[i]);
+		}
+		printf("]\n");
+	}
+	
+	//2: El proceso raiz envi­a una porcion del arreglo a cada proceso
+	MPI_Scatter(array, PORTION_SIZE, MPI_INT, recv_portion, PORTION_SIZE, MPI_INT, ROOT, MPI_COMM_WORLD);
+	printf("\nPaso 2: Id Proceso %d recibe una porcion del arreglo: ", rank);
+	printf("[");
+	for (int i = 0; i < PORTION_SIZE; i++) {
+		printf(" %d", recv_portion[i]);
+		//cada proceso incrementa en uno cada elemento de la porcion recibida
+		recv_portion[i] += 1;			
+	}
+	printf("]\n");
+	
+	//3: Cada proceso envi­a una porcion del arreglo al proceso raiz
+	MPI_Gather(recv_portion, PORTION_SIZE, MPI_INT, gathered_array, PORTION_SIZE, MPI_INT, ROOT, MPI_COMM_WORLD);
+	
+	if (rank == ROOT) {
+		printf("\nPaso 3: Proceso raiz reune el arreglo:\n");
+		printf("[");
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			printf(" %d", gathered_array[i]);
+		}
+		printf("]\n");
+	}
+	
+	MPI_Finalize();
+	return 0;
+}
